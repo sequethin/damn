eb0f49cd97de6d241283667689b50d699bdc7ae4
Mike Hernandez <mhernandez@etsy.com>
2015-11-15 17:57:14 -0500

    some more simple tests

--- test_tagReader.py
+++ test_tagReader.py
@@ -6,3 +6,6 @@ class TestTagReader(TestCase):
     def test_should_add(self):
         reader = TagReader()
         self.assertFalse(reader.should_add('bitrate_mode', mutagen.mp3.BitrateMode.UNKNOWN))
+        self.assertFalse(reader.should_add('channels', 2))
+        self.assertFalse(reader.should_add('arbitrary_tag', None))
+        self.assertFalse(reader.should_add('arbitrary_tag', False))
